f0c458f1e0df7a3ce0d6f4cb10232542e58b663a
sanchia-menezes <sanchia.menezes19@gmail.com>
2026-02-25 11:41:45 -0500

    Added terraform main and variables

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" { 
+}
+
+variable "app_name" {
+    description = "Name of the app"
+    default = "vendor-network"
+}
+
+variable "public_subnets" {
+  default = ["10.10.100.1/24", "10.10.101.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.10.0.1/24", "10.10.1.0/24"]
+}
+
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
